81930ce316c89451bf595c7eec92f841b7efe9cc
Akash143l3 <Team3Nala@gmail.com>
2025-04-05 09:35:27 +0530

    set-up code

---
+++ src/app/api/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      page
+      <Button>Click</Button>
+    </div>
+  );
+}
